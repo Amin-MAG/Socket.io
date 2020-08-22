5fdd53b2d6ee331e74c43f31687e575b97ed752f
Amin-MAG <M.Amin.Ghasvari@Gmail.com>
2020-08-23 02:08:06 +0430

    Before Socket.IO

---
+++ 01 Before Socket.IO/server/server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const websocket = require('ws');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+
+
+const server = http.createServer((req, res)=> {
+    console.log("Server Created.");
+    res.end("HelloWorld");
+});
+
+// WebSocket
+
+const wss = new websocket.Server({server});
+
+wss.on('headers', (headers, req) => {
+    console.log("Header: " + headers);
+    console.log("Req: " + req);
+});
+
+wss.on('connection', (ws, req) => {
+    // Send meesage from the server.
+    ws.send("Here is a message from the server!");
+    // Listener for recieving
+    ws.on('message', (msg) => {
+        console.log(msg);
+    });
+});
+
+// Run server
+
+server.listen(PORT, HOSTNAME, ()=> {
+    console.log("Server Listening On " + HOSTNAME + ":" +  PORT + ".");
+});
